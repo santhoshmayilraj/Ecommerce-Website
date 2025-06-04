4e2ce87de211b4180e298859d58d052ce39be67a
Santhosh <136286484+santhoshmayilraj@users.noreply.github.com>
2025-06-04 19:52:31 +0530

    the project
    this has till the custom bed part

---
+++ front_end/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ContactUs from './components/ContactUs';
+import AboutUs from './pages/AboutUs';
+import Home from './pages/HomePage';
+import Products from './pages/Products';
+import PaymentPage from './pages/PaymentPage';
+import CustomBedCreator from './pages/CustomBedCreator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/products" element={<Products />} /> 
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/custom-bed-creator" element={<CustomBedCreator />} />
+        </Routes>
+      </main>
+      <ContactUs />
+    </div>
+  );
+}
+
+export default App;
